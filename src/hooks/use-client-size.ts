893d08f8f2ee2e795058826302ae77841fac36a6
Blankeos <carloantonioct@gmail.com>
2024-06-09 20:48:56 +0800

    feat: Added solidjs helpers for resize.

---
+++ src/hooks/use-client-size.ts
@@ -0,0 +1,37 @@
+import { createEffect, createSignal } from 'solid-js';
+
+/**
+ * Using signals as refs: https://docs.solidjs.com/concepts/refs (Important because ref won't work if you wrap the element in a conditional).
+ * Resize Observer: https://stackoverflow.com/questions/5570390/resize-event-for-textarea
+ */
+export function useClientSize() {
+  const [ref, setRef] = createSignal<HTMLElement>();
+  const [width, setWidth] = createSignal(0);
+  const [height, setHeight] = createSignal(0);
+
+  const handleClientResize = () => {
+    if (!ref()) return;
+
+    console.log('moving.');
+
+    const rect = ref()!.getBoundingClientRect();
+
+    setWidth(() => rect.width);
+    setHeight(() => rect.height);
+  };
+
+  createEffect(() => {
+    if (!ref()) return;
+
+    // component is mounted and window is available
+    handleClientResize();
+
+    const resizeObserver = new ResizeObserver(handleClientResize);
+    resizeObserver.observe(ref()!);
+
+    // unsubscribe from the event on component unmount
+    return () => resizeObserver.disconnect();
+  });
+
+  return { width, height, clientRef: (el: HTMLElement | any) => setRef(el) };
+}
---
+++ src/hooks/use-window-size.ts
@@ -0,0 +1,24 @@
+import { createEffect, createSignal } from 'solid-js';
+
+/** https://stackoverflow.com/questions/74987753/how-can-i-react-to-changes-in-the-window-size-in-solidjs */
+export function useWindowSize() {
+  const [width, setWidth] = createSignal(0);
+  const [height, setHeight] = createSignal(0);
+
+  const handleWindowResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  createEffect(() => {
+    // component is mounted and window is available
+    handleWindowResize();
+
+    window.addEventListener('resize', handleWindowResize);
+
+    // unsubscribe from the event on component unmount
+    return () => window.removeEventListener('resize', handleWindowResize);
+  });
+
+  return { width, height };
+}
