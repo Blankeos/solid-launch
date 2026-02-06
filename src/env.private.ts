2ee82eeb64705445d5ad41b73745fd6b4aa625d5
Blankeos <carloantonioct@gmail.com>
2026-02-07 01:46:55 +0800

    chore: port is string to number.
    Signed-off-by: Blankeos <carloantonioct@gmail.com>

--- src/env.private.ts
+++ src/env.private.ts
@@ -5,7 +5,7 @@ export const privateEnv = createEnv({
   runtimeEnv: process.env,
   server: {
     /** Development|Prod. Port of the app. */
-    PORT: z.number().default(3000),
+    PORT: z.preprocess(Number, z.number()).default(3000),
     /** Development|Prod. */
     NODE_ENV: z.enum(["development", "production"]).default("development"),
 
