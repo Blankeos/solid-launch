32b553aa3c40c324356eb853992dd6ab618b313d
Blankeos <carloantonioct@gmail.com>
2026-03-22 09:35:22 +0800

    fix: env vars for t3oss vite8 (must be explicit).

--- src/env.public.ts
+++ src/env.public.ts
@@ -2,11 +2,14 @@ import { createEnv } from "@t3-oss/env-core"
 import z from "zod"
 
 export const publicEnv = createEnv({
-  runtimeEnv: import.meta.env,
   clientPrefix: "PUBLIC_",
   client: {
     // Add client envs here.
     PUBLIC_BASE_URL: z.string().default("http://localhost:3000"),
+  },
+  runtimeEnvStrict: {
+    PUBLIC_BASE_URL: import.meta.env.PUBLIC_BASE_URL,
+    NODE_ENV: import.meta.env.NODE_ENV,
   },
   server: {
     /** Development|Prod. */
